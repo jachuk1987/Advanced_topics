2d02ae036e46ae3a48f2d0c95c5a6d2263049938
jeswanth <jachu.k1212@gmail.com>
2025-01-30 12:29:38 +0530

    EmployeeDashboard

---
+++ employee-dashboard/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import EmployeeDashboard from './Components/EmployeeDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <EmployeeDashboard />
+    </div>
+  );
+}
+
+export default App;
