a2544588f09fa308cccace65b439ffbbc840c516
jeswanth <jachu.k1212@gmail.com>
2025-01-30 11:13:00 +0530

    WorkingonReactJs

--- AdvanceTopics-ReactJs/src/App.jsx
+++ AdvanceTopics-ReactJs/src/App.jsx
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import DigitalAddressBook from './Components/DigitalAddressBoook';
+import TrafficSignal from './Components/TrafficSignal';
 
 function App() {
   return (
     <div className="App">
      <DigitalAddressBook/>
+     <TrafficSignal />
     </div>
   );
 }
