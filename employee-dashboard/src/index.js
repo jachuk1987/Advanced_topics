82b452201668c143230462f2b527c3e5f87bf612
jeswanth <jachu.k1212@gmail.com>
2025-01-31 16:09:58 +0530

    Employee_Dashboard

--- employee-dashboard/src/index.js
+++ employee-dashboard/src/index.js
@@ -4,3 +4,10 @@ import App from "./App";
 import "antd/dist/reset.css"; // Fix compatibility issues
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// import "antd/dist/reset.css"; // Fix compatibility issues
+
+// ReactDOM.render(<App />, document.getElementById("root"));
