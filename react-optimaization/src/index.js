35f436969e60eb05de2312075d173253cfa82630
jeswanth <jachu.k1212@gmail.com>
2025-02-15 12:39:29 +0530

    React_Optimaization

--- react-optimaization/src/index.js
+++ react-optimaization/src/index.js
@@ -11,7 +11,8 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>  </React.StrictMode>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
