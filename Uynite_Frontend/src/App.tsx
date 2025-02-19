86b8cfc5f3876fb141829e40f3b6f2a8a9e47a63
jeswanth <jachu.k1212@gmail.com>
2025-02-19 13:12:24 +0530

    Uynite_Frontend

--- Uynite_Frontend/src/App.tsx
+++ Uynite_Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminHeader from "./Components/AdminHeader";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import CelebrityRequest from "./Components/CelebrityRequest/CelebrityRequest";
+import SponsoredEvent from "./Components/SponsoredEvent/SponsoredEvent";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/celebrity-request" element={<CelebrityRequest />} />
+        <Route path="/sponsored-event" element={<SponsoredEvent />} />
       </Routes>
     </Router>
   );
