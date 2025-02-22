ffa558ecaf1b44b11f182445a62dde1967a4106f
jeswanth <jachu.k1212@gmail.com>
2025-02-22 16:23:05 +0530

    JobSearch_platform

--- job-search-platform/src/App.js
+++ job-search-platform/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import SearchBar from './Components/SearchBar';
+import UserProfile from './Components/UserProfile';
+import Notifications from './Components/Notifications';
+import JobList from './Components/JobList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="max-w-4xl mx-auto p-6">
+        <h1 className="text-3xl font-bold text-center mb-8">Job Search Platform</h1>
+        <SearchBar />
+        <UserProfile />
+        <Notifications />
+        <JobList />
+      </div>
+    </Provider>
   );
 }
 
