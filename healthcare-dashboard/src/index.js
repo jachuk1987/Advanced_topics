47d64ed1a61b9c53b4f8ad39ae70e12d46f3717e
jeswanth <jachu.k1212@gmail.com>
2025-02-25 08:40:52 +0530

    HealthCare_Dashboard

--- healthcare-dashboard/src/index.js
+++ healthcare-dashboard/src/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider, Theme } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-// Optional: Extend theme to ensure _config is defined
-const theme = extendTheme({
-  config: {
-    initialColorMode: 'light',
-    useSystemColorMode: false,
-  },
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={Theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
