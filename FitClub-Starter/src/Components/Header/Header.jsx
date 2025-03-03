42a7bdd62686fa1d25222229f40f0f994aeaaef6
jeswanth <jachu.k1212@gmail.com>
2025-03-03 22:23:43 +0530

    Header

--- FitClub-Starter/src/Components/Header/Header.jsx
+++ FitClub-Starter/src/Components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div className="header">
         <img src={Logo} alt=''className='logo'/>
 
-        <ul>
+        <ul className='header-menu'>
           <li>Home</li>
           <li>Programs</li>
           <li>Why us</li>
