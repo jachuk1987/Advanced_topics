c9ae5ae5ccbe2a03643fc13f072dccc804bdd760
jeswanth <jachu.k1212@gmail.com>
2025-03-04 16:33:35 +0530

    FitClub-Starter

--- FitClub-Starter/src/App.js
+++ FitClub-Starter/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Hero from './Components/Hero/Hero';
+import Programs from './Components/Programs/Programs';
+
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Programs />
     </div>
   );
 }
