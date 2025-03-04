290b5be1066c7536f8931db3a6314ff01c9d52c0
jeswanth <jachu.k1212@gmail.com>
2025-03-04 11:08:21 +0530

    FitClub-Starter

--- FitClub-Starter/src/Components/Hero/Hero.jsx
+++ FitClub-Starter/src/Components/Hero/Hero.jsx
@@ -6,6 +6,11 @@ const Hero = () => {
         <div className="hero">
             <div className="left-h">
                 <Header />
+
+                <div className='the-best-ad'>
+                    <div></div>
+                    <span>the best fitness club in the town</span>
+                </div>
             </div>
             <div className="right-h">right side</div>
         </div>
