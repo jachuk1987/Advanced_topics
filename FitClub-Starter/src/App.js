489a04ec366261d8d72daafad66b5dc60208d5f6
jeswanth <jachu.k1212@gmail.com>
2025-03-05 06:24:26 +0530

    FitClub-Starter

--- FitClub-Starter/src/App.js
+++ FitClub-Starter/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import Hero from './Components/Hero/Hero';
+import Plans from './Components/Plans/Plans';
 import Programs from './Components/Programs/Programs';
 import Reasons from './Components/Reasons/Reasons';
+import Testimonials from './Components/Testimonials/Testimonials';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Hero />
       <Programs />
       <Reasons />
+      <Plans />
+      <Testimonials />
     </div>
   );
 }
