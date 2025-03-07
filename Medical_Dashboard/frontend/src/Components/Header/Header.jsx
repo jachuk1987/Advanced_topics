04411d35c9630fa03d9fa5f282b44d202cbead52
jeswanth <jachu.k1212@gmail.com>
2025-03-07 13:35:01 +0530

    Header

--- Medical_Dashboard/frontend/src/Components/Header/Header.jsx
+++ Medical_Dashboard/frontend/src/Components/Header/Header.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import logo from '../../assets/images/logo.png';
+import { NavLink, Link } from "react-router-dom";
 
+const navLinks = [
+  {
+    path: "/home",
+    display: "Home"
+  },
+  {
+    path: "/doctors",
+    display: "Find a Doctor"
+  },
+  {
+    path: "/services",
+    display: "Services"
+  },
+  {
+    path: "/contact",
+    display: "Contact"
+  }
+]
 const Header = () => {
   return <header className='header flex items-center'>
     <div className='container'>
@@ -8,6 +27,27 @@ const Header = () => {
         <div>
           <img src={logo} alt="" />
         </div>
+
+        <div className='navigation'>
+          <ul className="menu flex items-center gap-[2.7rem]">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink
+                  to={link.path}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-primaryColor text-[16px] leading-7 font-[600]"
+                      : "text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor"
+                  }
+                >
+                  {link.display}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        
       </div>
     </div>
   </header>
