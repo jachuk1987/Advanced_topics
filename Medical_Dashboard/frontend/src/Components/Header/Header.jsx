244e3aa74b70ba04499ea115ed25a4ca6f39d946
jeswanth <jachu.k1212@gmail.com>
2025-03-07 09:55:30 +0530

    Medical_Dashboard

--- Medical_Dashboard/frontend/src/Components/Header/Header.jsx
+++ Medical_Dashboard/frontend/src/Components/Header/Header.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import logo from '../../assets/images/logo.png';
 
 const Header = () => {
-  return (
-    <div>
-      Header
+  return <header className='header flex items-center'>
+    <div className='container'>
+      <div className='flex items-center justify-between'>
+        <div>
+          <img src={logo} alt="" />
+        </div>
+      </div>
     </div>
-  )
-}
+  </header>
+};
 
 export default Header;
