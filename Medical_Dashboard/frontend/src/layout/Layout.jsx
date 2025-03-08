43b239ff9fe86fce7c34c507c4c715dbfb5486d9
jeswanth <jachu.k1212@gmail.com>
2025-03-08 13:39:04 +0530

    Home_page

--- Medical_Dashboard/frontend/src/layout/Layout.jsx
+++ Medical_Dashboard/frontend/src/layout/Layout.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from "../Components/Header/Header";
 import Footer from '../Components/Footer/Footer';
 import Routers from '../routes/Routers';
+import Home from '../pages/Home';
 
 const Layout = () => {
     return (
         <>
             <Header />
+            <Home />
             <main>
                 <Routers />
             </main>
