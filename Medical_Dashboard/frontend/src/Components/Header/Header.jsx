fcf375e48e1e7fa06d0368014d3003b16418d956
jeswanth <jachu.k1212@gmail.com>
2025-03-08 10:26:54 +0530

    Medical_Dashboard

--- Medical_Dashboard/frontend/src/Components/Header/Header.jsx
+++ Medical_Dashboard/frontend/src/Components/Header/Header.jsx
@@ -41,7 +41,9 @@ const Header = () => {
     handleStickyHeader()
 
     return () => window.removeEventListener('scroll', handleStickyHeader)
-  })
+  });
+
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
 
   return <header className='header flex items-center' ref={headerRef}>
     <div className='container'>
@@ -50,7 +52,7 @@ const Header = () => {
           <img src={logo} alt="" />
         </div>
 
-        <div className='navigation'>
+        <div className='navigation' ref={menuRef} onClick={toggleMenu}>
           <ul className="menu flex items-center gap-[2.7rem]">
             {navLinks.map((link, index) => (
               <li key={index}>
@@ -82,7 +84,7 @@ const Header = () => {
             <button className='bg-primaryColor py-2 px-6 text-white font-[600] h-[44px]flex items-center justify-center rounded-[50px]'>Login</button>
           </Link>
 
-            <span className='md:hidden'>
+            <span className='md:hidden' onClick={toggleMenu}>
               <BiMenu className='w-6 h-6 cursor-pointer'/>
             </span>
         </div>
