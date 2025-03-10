ab0f897b9ff5832ff9f857a4d27e315a38a76b42
jeswanth <jachu.k1212@gmail.com>
2025-03-10 11:48:30 +0530

    ServiceList&Card

--- Medical_Dashboard/frontend/src/pages/Home.jsx
+++ Medical_Dashboard/frontend/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import icon03 from "../assets/images/icon03.png";
 import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs"
 import About from '../Components/About/About';
+import ServiceList from '../Components/Services/ServiceList';
 
 const Home = () => {
   return <>
@@ -163,6 +164,8 @@ const Home = () => {
           <h2 className='heading text-center'>Our medical services</h2>
           <p className='text__para text-center'>World-class care for everyone. Our health System offers unmatched, expert health care.</p>
         </div>
+
+        <ServiceList />
       </div>
     </section>
     {/*========= Services section ========== */}
