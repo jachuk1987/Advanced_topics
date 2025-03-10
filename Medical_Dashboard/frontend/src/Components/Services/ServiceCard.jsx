d19e9cf2abe90295355f7c53684fa51d0ccd7106
jeswanth <jachu.k1212@gmail.com>
2025-03-10 10:40:17 +0530

    ServiceCard

---
+++ Medical_Dashboard/frontend/src/Components/Services/ServiceCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ServiceCard = ({ item, index }) => {
+
+    const { name, desc, bgColor, textColor } = item
+
+    return (<div className='py-[30px] px-3 lg:px-5'>
+
+    </div>
+    );
+};
+
+export default ServiceCard;
