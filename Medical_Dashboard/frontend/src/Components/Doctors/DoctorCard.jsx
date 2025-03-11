4348d2336294b1a382841ffa0f6c3ab43e68bbd8
jeswanth <jachu.k1212@gmail.com>
2025-03-11 15:16:43 +0530

    Home_page

--- Medical_Dashboard/frontend/src/Components/Doctors/DoctorCard.jsx
+++ Medical_Dashboard/frontend/src/Components/Doctors/DoctorCard.jsx
@@ -42,11 +42,23 @@ const DoctorCard = ({ doctor }) => {
                 </div>
             </div>
 
-        <div className='mt-[18px] lg:mt-5 flex items-center justify-between'>
-            <div>
-                <h3></h3>
+            <div className='mt-[18px] lg:mt-5 flex items-center justify-between'>
+                <div>
+                    <h3 className='text-[16px] leading-7 lg:text-[18px] lg:leading-[30px] font-semibold text-headingColor'>
+                        + {totalPatient} patients
+                    </h3>
+                    <p className='text-[14px] leading-6 font-[400] text-textColor'>
+                        At {hospital}
+                    </p>
+                </div>
+
+                <Link
+                    to={'/doctors'} 
+                    className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] flex items-center justify-center group hover:bg-primaryColor hover:border-none'
+                >
+                    <BsArrowRight className='group-hover:text-white w-6 h-5' />
+                </Link>
             </div>
-        </div>
         </div>
     );
 };
--- Medical_Dashboard/frontend/src/pages/Home.jsx
+++ Medical_Dashboard/frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import heroImg01 from "../assets/images/hero-img01.png";
 import heroImg02 from "../assets/images/hero-img02.png";
 import heroImg03 from "../assets/images/hero-img03.png";
@@ -6,6 +7,7 @@ import icon01 from "../assets/images/icon01.png";
 import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
 import featureImg from "../assets/images/feature-img.png";
+import faqImg from "../assets/images/faq-img.png";
 import videoIcon from "../assets/images/video-icon.png";
 import avatarIcon from "../assets/images/avatar-icon.png";
 import { Link } from "react-router-dom";
@@ -64,7 +66,7 @@ const Home = () => {
 
           </div>
 
-          {/*======= Hero Content ======== */}
+          {/*======= Hero Content end ======== */}
           <div className='flex gap-[30px] justify-end'>
             <div>
               <img className='w-full' src={heroImg01} alt="" />
@@ -107,7 +109,10 @@ const Home = () => {
                 unmatched, expert health care. From the lab to the clinic.
               </p>
 
-              <Link to={'/doctors'} className='w-[44px] h-[44px] rounded-full border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+              <Link
+                to={'/doctors'}
+                className='w-[44px] h-[44px] rounded-full border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'
+              >
                 <BsArrowRight className='group-hover:text-white w-6 h-5' />
               </Link>
             </div>
@@ -236,7 +241,7 @@ const Home = () => {
         </div>
       </div>
     </section>
-    {/*========= feature section ========== */}
+    {/*========= feature section end ========== */}
 
     {/*========= our great doctors ========== */}
     <section>
@@ -247,11 +252,21 @@ const Home = () => {
             World-class care for everyone. Our health System offers unmatched, expert health care.
           </p>
         </div>
-        
+
         <DoctorList />
       </div>
     </section>
-    {/*========= our great doctors ========== */}
+    {/*========= our great doctors end ========== */}
+
+    {/*========= faq section ========== */}
+    <section>
+      <div className='container'>
+        <div className='flex justify-between gap-[50px] lg:gap-0'>
+          <div className='w-1/2 hidden md:block'><img src={faqImg} alt="" /></div>
+        </div>
+      </div>
+    </section>
+    {/*========= faq section end ========== */}
 
   </>
 }
