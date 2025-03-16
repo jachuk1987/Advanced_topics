1c7a24e09341a4425b0661b7a40d03b875f741b4
jeswanth <jachu.k1212@gmail.com>
2025-03-16 14:00:52 +0530

    DoctorAbout_page

--- Medical_Dashboard/frontend/src/pages/Doctors/DoctorAbout.jsx
+++ Medical_Dashboard/frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 const DoctorAbout = () => {
   return (
     <div>
-      
+      <div>
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2">
+          About of
+          <span className="text-irisBlueColor font-bold text-[24px] leading-9">
+            Muhibur Rahman
+          </span>
+        </h3>
+        <p className="text__para">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis eius assumenda corrupti at fugiat ipsum odio laudantium quisquam veritatis
+          consectetur velit illo ullam animi necessitatibus vero voluptatum fuga consequuntur, aspernatur perspiciatis adipisci. Necessitatibus et non
+          sapiente sit distinctio, repellat illo totam perspiciatis, inventore ex assumenda odit natus cumque saepe nostrum?
+        </p>
+      </div>
+
     </div>
   )
 }
