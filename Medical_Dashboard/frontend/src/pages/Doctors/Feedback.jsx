cb58f3c6c15c442e91e5b5828bf766ce743a4a04
jeswanth <jachu.k1212@gmail.com>
2025-03-17 13:36:15 +0530

    Doctors_page

--- Medical_Dashboard/frontend/src/pages/Doctors/Feedback.jsx
+++ Medical_Dashboard/frontend/src/pages/Doctors/Feedback.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import avatar from "../../assets/images/avatar-icon.png";
 import { formateDate } from "../../utils/formateDate";
 import { AiFillStar } from "react-icons/ai";
+import FeedbackForm from "./FeedbackForm";
 
 const Feedback = () => {
 
@@ -40,9 +41,15 @@ const Feedback = () => {
         </div>
       </div>
 
-      <div className="text-center">
-          <button className="btn">Give Feedback</button>
-      </div>
+      {!showFeedbackForm && (
+        <div className="text-center">
+          <button className="btn" onClick={() => setShowFeedbackForm(true)}>
+            Give Feedback
+          </button>
+        </div>
+      )}
+
+      {showFeedbackForm && <FeedbackForm />}
     </div>
   );
 };
