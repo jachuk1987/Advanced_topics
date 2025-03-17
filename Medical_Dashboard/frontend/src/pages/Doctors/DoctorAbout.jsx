e1afba41b12ca805e06441dbefdbdeb2a4ea4a00
jeswanth <jachu.k1212@gmail.com>
2025-03-17 13:25:08 +0530

    Doctors_page

--- Medical_Dashboard/frontend/src/pages/Doctors/DoctorAbout.jsx
+++ Medical_Dashboard/frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { formateDate } from '../../utils/formateDate';
 
 const DoctorAbout = () => {
@@ -27,7 +26,7 @@ const DoctorAbout = () => {
           <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
             <div>
               <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
-              {formateDate("07-24-2020")} - {formateDate("07-24-2022")}
+                {formateDate("07-24-2020")} - {formateDate("07-24-2022")}
               </span>
               <p className="text-[16px] leading-6 font-medium text-textColor">
                 PHD in Surgeon
@@ -47,6 +46,42 @@ const DoctorAbout = () => {
                 PHD in Surgeon
               </p>
             </div>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, New York.
+            </p>
+          </li>
+        </ul>
+      </div>
+
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+          Experience
+        </h3>
+
+        <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {formateDate("07-04-2010")} - {formateDate("08-13-2014")}
+            </span>
+
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
+
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, New York.
+            </p>
+          </li>
+
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {formateDate("07-04-2010")} - {formateDate("08-13-2014")}
+            </span>
+
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
+
             <p className="text-[14px] leading-5 font-medium text-textColor">
               New Apollo Hospital, New York.
             </p>
