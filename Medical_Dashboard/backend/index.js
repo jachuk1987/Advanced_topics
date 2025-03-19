105d1ec8e781851f73dd2757f2254f9572b2833d
jeswanth <jachu.k1212@gmail.com>
2025-03-19 17:46:19 +0530

    Backend

--- Medical_Dashboard/backend/index.js
+++ Medical_Dashboard/backend/index.js
@@ -17,6 +17,11 @@ app.get("/", (req, res) => {
     res.send("Api is Working");
 });
 
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
 app.listen(port, () => {
     console.log("Server is running on port" + port);
-})
+});
