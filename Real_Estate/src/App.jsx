2977a6a900386a78a534ba32f8a910b6a59efac7
jeswanth <jachu.k1212@gmail.com>
2025-03-26 20:38:40 +0530

    Real_Estate

--- Real_Estate/src/App.jsx
+++ Real_Estate/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar';
+import Header from './Components/Header';
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   )
 }
