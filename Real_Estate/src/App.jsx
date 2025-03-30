a08f83ab04dd323b89f1d94d51e9fe359f728dce
jeswanth <jachu.k1212@gmail.com>
2025-03-30 13:52:00 +0530

    Real_Estate

--- Real_Estate/src/App.jsx
+++ Real_Estate/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Components/Header';
+import About from './Components/About';
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
       <Header />
+      <About />
     </div>
   )
 }
