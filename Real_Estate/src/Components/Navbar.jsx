372ed7865f83adad3c5a5e802f9d2bf0831a4644
jeswanth <jachu.k1212@gmail.com>
2025-03-30 12:38:19 +0530

    Real_Estate

--- Real_Estate/src/Components/Navbar.jsx
+++ Real_Estate/src/Components/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { assets } from "../assets/assets";
 
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false)
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
       <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
@@ -17,7 +18,7 @@ const Navbar = () => {
       </div>
 
       {/*---------- mobile-menu--------- */}
-      <div className='md:hidden fixed w-full right-0 top-0 bottom-0 overflow-hidden bg-white transition-all'>
+      <div className={`md:hidden ${showMobileMenu ? 'fixed w-full' : 'h-0 w-0'} right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
         <div className='flex justify-end p-6 cursor-pointer'>
           <img src={assets.cross_icon} className='w-6' alt="" />
         </div>
