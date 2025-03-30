e269142659be2dbaec8c26a61f23ff39bc490d0b
jeswanth <jachu.k1212@gmail.com>
2025-03-30 12:57:18 +0530

    Real_Estate

--- Real_Estate/src/Components/Navbar.jsx
+++ Real_Estate/src/Components/Navbar.jsx
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { assets } from "../assets/assets";
 
 const Navbar = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false)
+
+  useEffect(() => {
+    if(showMobileMenu){
+      document.body.style.overflow = 'hidden'
+    }else{
+      document.body.style.overflow = 'auto'
+    }
+    return ()=> {
+      document.body.style.overflow = 'auto'
+    };
+  }, [showMobileMenu])
+
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
       <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
@@ -20,7 +32,7 @@ const Navbar = () => {
       {/*---------- mobile-menu--------- */}
       <div className={`md:hidden ${showMobileMenu ? 'fixed w-full' : 'h-0 w-0'} right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
         <div className='flex justify-end p-6 cursor-pointer'>
-          <img src={assets.cross_icon} className='w-6' alt="" />
+          <img onClick={()=> setShowMobileMenu(false)} src={assets.cross_icon} className='w-6' alt="" />
         </div>
         <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
           <a href="#Header" className='px-4 py2 rounded-full inline-block'>Home</a>
