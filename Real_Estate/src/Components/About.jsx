597bffee922bf3e72fed4bafdb94c71dfc419d72
jeswanth <jachu.k1212@gmail.com>
2025-04-01 11:03:56 +0530

    Real_Estate

--- Real_Estate/src/Components/About.jsx
+++ Real_Estate/src/Components/About.jsx
@@ -27,7 +27,8 @@ const About = () => {
               <p>Ongoing Projects</p>
             </div>
           </div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum veniam quam cumque consectetur labore adipisci impedit repudiandae tempore id commodi ullam voluptate perspiciatis perferendis, maiores unde dolorum asperiores, voluptates mollitia!</p>
+          <p className='my-10 max-w-lg'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry,s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <button>Learn more</button>
         </div>
       </div>
     </div>
