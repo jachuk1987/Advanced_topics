ce7b24c3d5d69f7ac21bee563f9f0feaaac653e0
jeswanth <jachu.k1212@gmail.com>
2025-04-10 20:18:10 +0530

    Admin_Dashboard

--- admin_dashboard/src/App.jsx
+++ admin_dashboard/src/App.jsx
@@ -1,6 +1,8 @@
+import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Sidebar from "./Components/Sidebar";
 import { createContext } from "react";
+import Dashboard from "./pages/Dashboard";
 
 const MyContext = createContext();
 
