66eb357e793f6b82ffe1ed03b35f0327602b7e3b
jeswanth <jachu.k1212@gmail.com>
2025-04-11 23:05:48 +0530

    Admin_Dashboard

--- admin_dashboard/src/Components/Sidebar/index.jsx
+++ admin_dashboard/src/Components/Sidebar/index.jsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
   return (
     <>
       <div className='sidebar fixed top-0 left-0 z-[100] w-[15%]'>
-        <div className='logoWrapper d-none'>
+        <div className='logoWrapper py-4 px-5'>
           <img src={Logo} className='w-100' />
         </div>
       </div>
