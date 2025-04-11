1c56d9ad1fb9aa4287e482b5163a2b2579a2fcce
jeswanth <jachu.k1212@gmail.com>
2025-04-11 23:09:14 +0530

    Admin_Dashboard

--- admin_dashboard/src/Components/Sidebar/index.jsx
+++ admin_dashboard/src/Components/Sidebar/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Logo from "../../assets/images/logo.png";
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
     <>
       <div className='sidebar fixed top-0 left-0 z-[100] w-[15%]'>
-        <div className='logoWrapper py-4 px-5'>
-          <img src={Logo} className='w-100' />
-        </div>
+        <Link to="/">
+          <div className='logoWrapper py-4 px-3'>
+            <img src={Logo} className='w-100' />
+          </div>
+        </Link>
       </div>
     </>
   )
