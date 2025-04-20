10a90309d9eca7effbc5d5efb5f134a36aab73c5
jeswanth <jachu.k1212@gmail.com>
2025-04-20 07:10:31 +0530

    Data_Dashboard

--- data-dashboard/src/charts/BarChartComp.tsx
+++ data-dashboard/src/charts/BarChartComp.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import {
+  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
+} from 'recharts';
 
 interface BarChartProps {
-  data: any[];
+  data: { name: string; value: number }[];
 }
 
-const BarChartComp: React.FC<BarChartProps> = ({ data }) => (
-  <ResponsiveContainer width="100%" height={300}>
-    <BarChart data={data}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Bar dataKey="value" fill="#82ca9d" />
-    </BarChart>
-  </ResponsiveContainer>
-);
+const BarChartComp: React.FC<BarChartProps> = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey="value" fill="#82ca9d" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
 
 export default BarChartComp;
