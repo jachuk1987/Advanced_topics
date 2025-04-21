61d8ee356ce7cecd3674ed8beec39005a338177c
jeswanth <jachu.k1212@gmail.com>
2025-04-21 10:01:57 +0530

    Data_Dashboard

--- data-dashboard/src/components/Auth/PrivateRoute.tsx
+++ data-dashboard/src/components/Auth/PrivateRoute.tsx
@@ -1,14 +1,35 @@
+// import React from "react";
+// import { Navigate, useLocation } from "react-router-dom";
+
+// interface PrivateRouteProps {
+//   children: JSX.Element;
+// }
+
+// const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
+//   const location = useLocation();
+//   const isAuthenticated = Boolean(localStorage.getItem("authToken"));
+
+//   return isAuthenticated ? (
+//     children
+//   ) : (
+//     <Navigate to="/login" replace state={{ from: location }} />
+//   );
+// };
+
+// export default PrivateRoute;
+
+
 import React from "react";
 import { Navigate } from "react-router-dom";
 
 interface PrivateRouteProps {
-  children: JSX.Element;
+  children: React.ReactNode;
 }
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
   const isAuthenticated = !!localStorage.getItem("authToken");
 
-  return isAuthenticated ? children : <Navigate to="/login" />;
+  return isAuthenticated ? <>{children}</> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
