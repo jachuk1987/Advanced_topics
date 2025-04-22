4b9f5cf1602153b9c6280bd68460e9e67274832d
jeswanth <jachu.k1212@gmail.com>
2025-04-22 12:43:58 +0530

    Data_Dashboard

--- data-dashboard/src/components/Overview.tsx
+++ data-dashboard/src/components/Overview.tsx
@@ -1,35 +1,35 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-import { Typography, Paper } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
 
 const Overview: React.FC = () => {
-    return (
-      <Grid container spacing={3}>
-        {/* Total Users */}
-        <Grid item xs={12} md={4}>
-          <Paper elevation={3} sx={{ p: 2 }}>
-            <Typography variant="h6">Total Users</Typography>
-            <Typography variant="h4">1,024</Typography>
-          </Paper>
-        </Grid>
-  
-        {/* Active Sessions */}
-        <Grid item xs={12} md={4}>
-          <Paper elevation={3} sx={{ p: 2 }}>
-            <Typography variant="h6">Active Sessions</Typography>
-            <Typography variant="h4">256</Typography>
-          </Paper>
-        </Grid>
-  
-        {/* Revenue */}
-        <Grid item xs={12} md={4}>
-          <Paper elevation={3} sx={{ p: 2 }}>
-            <Typography variant="h6">Revenue</Typography>
-            <Typography variant="h4">$12,540</Typography>
-          </Paper>
-        </Grid>
+  return (
+    <Grid container spacing={3}>
+      {/* Total Users */}
+      <Grid item xs={12} md={4}>
+        <Paper elevation={3} sx={{ p: 2 }}>
+          <Typography variant="h6">Total Users</Typography>
+          <Typography variant="h4">1,024</Typography>
+        </Paper>
       </Grid>
-    );
-  };
-  
-  export default Overview;
+
+      {/* Active Sessions */}
+      <Grid item xs={12} md={4}>
+        <Paper elevation={3} sx={{ p: 2 }}>
+          <Typography variant="h6">Active Sessions</Typography>
+          <Typography variant="h4">256</Typography>
+        </Paper>
+      </Grid>
+
+      {/* Revenue */}
+      <Grid item xs={12} md={4}>
+        <Paper elevation={3} sx={{ p: 2 }}>
+          <Typography variant="h6">Revenue</Typography>
+          <Typography variant="h4">$12,540</Typography>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Overview;
