ba33c37339349490cf5a285c049e6f17a98613d8
jeswanth <jachu.k1212@gmail.com>
2025-04-22 12:41:29 +0530

    Dash_Board

--- data-dashboard/src/components/Overview.tsx
+++ data-dashboard/src/components/Overview.tsx
@@ -1,32 +1,35 @@
 import React from "react";
-import { Grid, Typography, Paper } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import { Typography, Paper } from "@mui/material";
 
 const Overview: React.FC = () => {
-  return (
-    <Grid container spacing={3}>
-      {/* Summary Cards */}
-      <Grid item xs={12} md={4}>
-        <Paper elevation={3} style={{ padding: "16px" }}>
-          <Typography variant="h6">Total Users</Typography>
-          <Typography variant="h4">1,024</Typography>
-        </Paper>
+    return (
+      <Grid container spacing={3}>
+        {/* Total Users */}
+        <Grid item xs={12} md={4}>
+          <Paper elevation={3} sx={{ p: 2 }}>
+            <Typography variant="h6">Total Users</Typography>
+            <Typography variant="h4">1,024</Typography>
+          </Paper>
+        </Grid>
+  
+        {/* Active Sessions */}
+        <Grid item xs={12} md={4}>
+          <Paper elevation={3} sx={{ p: 2 }}>
+            <Typography variant="h6">Active Sessions</Typography>
+            <Typography variant="h4">256</Typography>
+          </Paper>
+        </Grid>
+  
+        {/* Revenue */}
+        <Grid item xs={12} md={4}>
+          <Paper elevation={3} sx={{ p: 2 }}>
+            <Typography variant="h6">Revenue</Typography>
+            <Typography variant="h4">$12,540</Typography>
+          </Paper>
+        </Grid>
       </Grid>
-
-      <Grid item xs={12} md={4}>
-        <Paper elevation={3} style={{ padding: "16px" }}>
-          <Typography variant="h6">Active Sessions</Typography>
-          <Typography variant="h4">256</Typography>
-        </Paper>
-      </Grid>
-
-      <Grid item xs={12} md={4}>
-        <Paper elevation={3} style={{ padding: "16px" }}>
-          <Typography variant="h6">Revenue</Typography>
-          <Typography variant="h4">$12,540</Typography>
-        </Paper>
-      </Grid>
-    </Grid>
-  );
-};
-
-export default Overview;
+    );
+  };
+  
+  export default Overview;
