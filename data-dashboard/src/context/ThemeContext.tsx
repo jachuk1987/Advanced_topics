a2704689d0d5bc0ae8a8b992e8edaa750106f4ce
jeswanth <jachu.k1212@gmail.com>
2025-04-24 21:41:34 +0530

    Data_Dashboard

--- data-dashboard/src/context/ThemeContext.tsx
+++ data-dashboard/src/context/ThemeContext.tsx
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useState } from "react";
 interface ThemeContextProps {
   darkMode: boolean;
   toggleTheme: () => void;
-}
+};
 
 const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
 
