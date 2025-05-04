39e3a1ca276182c3874a9ad29a9868961db3cb0a
jeswanth <jachu.k1212@gmail.com>
2025-05-04 09:04:18 +0530

    Weather_App

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState,useEffect } from "react";
+import Navbar from "../src/components/navbar";
+import MainWeatherCard from "../src/components/mainweathercard";
+import FiveDayForecast from "../src/components/fiveday";
+import TodayHighlights from "../src/components/todayhighlights";
 
-function App() {
-  const [count, setCount] = useState(0)
+import axios from "axios";
+
+const WeatherDashboard = () => {
+
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState('London'); // Default city is set to London
+  const [airQualityData, setAirQualityData] = useState(null);
+  const [fiveDayForecast, setFiveDayForecast] = useState(null);
+
+  useEffect(() => {
+    fetchWeatherData(city);
+  }, [city]);
+
+  const fetchAirQualityData = (lat, lon) => {
+    const API_KEY = 'Your Api Key'; // Replace with your OpenWeatherMap API key
+    axios.get(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+      .then(response => {
+        setAirQualityData(response.data.list[0]); // Set the first item in the list as air quality data
+      })
+      .catch(error => console.error('Error fetching the air quality data:', error));
+  };
+
+  const fetchWeatherData = (city) => {
+    const API_KEY = 'Your Api Key'; // Replace with your OpenWeatherMap API key
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
+      .then(response => response.json())
+      .then(data => {
+        setWeatherData(data);
+        console.log(JSON.stringify(data));
+        fetchAirQualityData(data.coord.lat, data.coord.lon); 
+        axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_KEY}`)
+        .then(response => {
+          setFiveDayForecast(response.data);
+        })
+        .catch(error => console.error('Error fetching the 5-day forecast data:', error));
+    
+      })
+      .catch(error => console.error('Error fetching the weather data:', error));
+  };
+
+  const handleSearch = (searchedCity) => {
+    setCity(searchedCity); 
+  };
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Navbar onSearch={handleSearch} />
+      {weatherData && airQualityData &&  (
+        <div style={{ display: "flex", padding: "30px", gap: "20px" }}>
+          <div style={{ flex: "1", marginRight: "10px" }}>
+            <MainWeatherCard weatherData={weatherData} />
+            <p style={{ fontWeight: "700", fontSize: "20px", marginTop: "20px" }}>5 Days Forecast</p>
+            {fiveDayForecast && <FiveDayForecast forecastData={fiveDayForecast} />}
+          </div>
+          <div style={{ display: "flex", flexDirection: "column", flex: "0.5", gap: "20px" }}>
+            <TodayHighlights weatherData={weatherData} airQualityData={airQualityData}  />
+            
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default App
+export default WeatherDashboard;
