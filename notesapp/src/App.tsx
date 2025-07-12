783242913ed77f432e3255fa0a752d3966f62a9c
jeswanth <jachu.k1212@gmail.com>
2025-07-12 21:00:14 +0530

    NotesApp_TypeScript

--- notesapp/src/App.tsx
+++ notesapp/src/App.tsx
@@ -1,10 +1,10 @@
 import Header from "./Components/Header";
 import
 
-function App() {
-  return (
-    <Header />
-  );
-}
+  function App() {
+    return (
+      <Header />
+    );
+  }
 
 export default App;
