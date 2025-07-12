566b9044507215d1e4fd0d5ff51ebe0f7811ff13
jeswanth <jachu.k1212@gmail.com>
2025-07-12 14:36:46 +0530

    NotesApp_TypeScript

--- notesapp/src/App.tsx
+++ notesapp/src/App.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Header";
+import
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Header />
   );
 }
 
