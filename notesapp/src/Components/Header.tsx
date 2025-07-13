0cda71feaedf737cfeed4e3f59d6983b1acd0bd4
jeswanth <jachu.k1212@gmail.com>
2025-07-13 13:39:18 +0530

    NotesApp_TypeScript

--- notesapp/src/Components/Header.tsx
+++ notesapp/src/Components/Header.tsx
@@ -1,11 +1,12 @@
-import { AppBar, Toolbar} from "@mui/material";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import { logo } from "../constants/constant";
 
 const Header: React.FC = () => {
     return (
         <AppBar color="transparent">
             <Toolbar>
-                <img src={logo} alt="logo" style={{width: 30}} />
+                <img src={logo} alt="logo" style={{width: 30, marginRight: 10}} />
+                <Typography>Everynote</Typography>
             </Toolbar>
         </AppBar>
     )
