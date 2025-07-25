ab751e58bd67c7a6a527bd3d2e6ff2ad9589acc1
jeswanth <jachu.k1212@gmail.com>
2025-07-25 11:18:45 +0530

    NotesApp_TypeScript

--- notesapp/src/App.tsx
+++ notesapp/src/App.tsx
@@ -2,8 +2,11 @@ import { Box } from "@mui/material";
 import CreateNote from "./Components/CreateNote";
 import Header from "./Components/Header";
 import Notes from "./Components/Notes";
+import { useState } from "react";
 
 function App() {
+
+  const [notes, setNotes] = useState();
 
   return (
     <>
