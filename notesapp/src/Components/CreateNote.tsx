241721042d0d79d57ffce5720089777231522280
jeswanth <jachu.k1212@gmail.com>
2025-07-26 09:54:06 +0530

    NotesApp_TypeScript

--- notesapp/src/Components/CreateNote.tsx
+++ notesapp/src/Components/CreateNote.tsx
@@ -41,6 +41,7 @@ interface ICreateNoteProps {
 
 const CreateNote: React.FC<ICreateNoteProps> = ({ addNotes }) => {
     const [note, setNote] = useState<NoteObject>(defaultObj);
+    const [error, setError] = useState('');
 
     const onValueChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
 
