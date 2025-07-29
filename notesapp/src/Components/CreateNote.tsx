a792480b6d4ff224e100874ba0a560d06694f484
jeswanth <jachu.k1212@gmail.com>
2025-07-29 06:51:55 +0530

    NotesApp_TypeScript

--- notesapp/src/Components/CreateNote.tsx
+++ notesapp/src/Components/CreateNote.tsx
@@ -4,28 +4,33 @@ import { title } from "process";
 import { useState } from "react";
 import { NoteObject } from "../models/note";
 import { v4 as uuid } from "uuid";
+import { DETAILS_LIMIT, TITLE_LIMIT } from "../constants/constant";
 
 const Container = styled(Box)`
-    & > * {
-        margin-right: 20px 20px 20px 0;
-    }
-    & > div > input[type="text] {
-        border-bottom: 1px solid #111111;
-        opacity: 0.4;
-        width: 300px;
-        padding-right: 25px;
-    }
-    & > div > input[type="color"] {
-        width: 40px;
-        height: 30px;
-        position: relative;
-        bottom: -10px;
-    }
-    & > span {
-        font-size: 10px;
-        position: relative;
-        right: 40px;
-    }
+  & > * {
+    margin: 20px 20px 20px 0;
+  }
+
+  & input[type="text"],
+  & input[type="textarea"] {
+    border-bottom: 1px solid #111111;
+    opacity: 0.4;
+    width: 300px;
+    padding-right: 25px;
+  }
+
+  & input[type="color"] {
+    width: 40px;
+    height: 30px;
+    position: relative;
+    bottom: -10px;
+  }
+
+  & > span {
+    font-size: 10px;
+    position: relative;
+    right: 40px;
+  }
 `;
 
 const Error = styled(Typography)`
@@ -74,15 +79,21 @@ const CreateNote: React.FC<ICreateNoteProps> = ({ addNotes }) => {
                 onChange={(e) => onValueChange(e)}
                 name="title"
                 value={note.title}
+                inputProps={
+                    { maxLength: TITLE_LIMIT }
+                }
             />
-            <Box component="span">30</Box>
+            <Box component="span">{note.title.length}/{TITLE_LIMIT}</Box>
             <InputBase
                 placeholder="Details"
                 onChange={(e) => onValueChange(e)}
                 name="details"
                 value={note.details}
+                inputProps={
+                    { maxLength: DETAILS_LIMIT }
+                }
             />
-            <Box component="span">50</Box>
+            <Box component="span">{note.details.length}/{DETAILS_LIMIT}</Box>
             <InputBase
                 type="color"
                 defaultValue={"#F5F5F5"}
