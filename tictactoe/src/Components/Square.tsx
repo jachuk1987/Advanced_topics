b2bb774ea7df25745860831dfcaf729fa66f7e1f
jeswanth <jachu.k1212@gmail.com>
2025-07-31 08:56:48 +0530

    TicTacToe_TypeScript

--- tictactoe/src/Components/Square.tsx
+++ tictactoe/src/Components/Square.tsx
@@ -2,15 +2,16 @@
 
 interface ISquareProps {
     onClick: () => void;
+    value: 'X' | 'O' | null
 }
 
-const Square: React.FC<ISquareProps> = ({ onClick }) => {
+const Square: React.FC<ISquareProps> = ({ onClick, value }) => {
     return (
         <button
             className="board-square"
             onClick={onClick}
         >
-            
+            {value}
         </button>
     )
 }
