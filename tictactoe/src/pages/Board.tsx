207f4783794a6f5cbed4631dd49b13ecfd642951
jeswanth <jachu.k1212@gmail.com>
2025-07-31 08:59:25 +0530

    TicTacToe_TypeScript

--- tictactoe/src/pages/Board.tsx
+++ tictactoe/src/pages/Board.tsx
@@ -17,7 +17,7 @@ const Board: React.FC = () => {
             return value;
         })
         setSquares(data);
-        setCurrentPlayer
+        setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
     }
 
     return (
